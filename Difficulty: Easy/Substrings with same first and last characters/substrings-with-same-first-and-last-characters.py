e46829624846b404e9f1375348ec52bb34e1bff1
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-05-15 10:00:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Substrings with same first and last characters/substrings-with-same-first-and-last-characters.py
@@ -0,0 +1,28 @@
+from collections import Counter
+
+class Solution:
+    def countSubstring(self, s):
+        cnt = 0
+        for ch in set(s):
+            f = s.count(ch)
+            cnt += (f * (f - 1)) // 2  # pairs (i < j) where s[i] == s[j]
+            cnt += f  # each single character
+        return cnt
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        s = input()
+
+        ob = Solution()
+        answer = ob.countSubstring(s)
+
+        print(answer)
+        print("~")
+
+# } Driver Code Ends
