2d256979df3813a86545c02a0a891d205e253d93
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-05-17 23:50:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort the given array after applying the given equation/sort-the-given-array-after-applying-the-given-equation.py
@@ -0,0 +1,32 @@
+class Solution:
+	def sortArray(self, arr, A, B, C):
+		# Code here
+		l=[]
+		for i in arr:
+		    l.append(A*(i**2)+B*i+C)
+		return sorted(l)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+        a = int(input())
+        b = int(input())
+        c = int(input())
+
+        ob = Solution()
+        ans = ob.sortArray(arr, a, b, c)
+        print(' '.join(map(str, ans)))
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
