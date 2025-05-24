1ff848b4f8473980f1e129b5fda6db1bd7bb275d
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-05-24 10:20:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of all substrings of a number/sum-of-all-substrings-of-a-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+     int sumSubstrings(string &s) {
+        // code here
+        int ans =0;
+        for(int i=0;i<s.size();i++) 
+         {
+             for(int j=i;j<s.size();j++)
+              {
+                  ans+=stoi(s.substr(i,j-i+1));
+              }
+         }
+         return ans;
+    }
+};
