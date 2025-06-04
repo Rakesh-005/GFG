b56ba325a1db89fb725a67a69541fd6961dacfd8
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-06-04 12:45:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.py
@@ -0,0 +1,14 @@
+# User function Template for python3
+
+class Solution:
+    def median(self, mat):
+        arr = []
+        for row in mat:
+            arr.extend(row)  # flatten the matrix
+        
+        arr.sort()
+        n = len(arr)
+        if (n & 1) == 0:
+            return (arr[n // 2] + arr[n // 2 - 1]) // 2  # integer division for median
+        else:
+            return arr[n // 2]
