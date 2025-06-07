086f694cccb19d6bc1d98600443b7c2962a98850
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-06-07 23:27:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Three way partitioning/three-way-partitioning.py
@@ -0,0 +1,8 @@
+#User function template for Python
+
+class Solution:
+    #Function to partition the array around the range such 
+    #that array is divided into three parts.
+	def threeWayPartition(self, arr, a, b):
+	    # code here 
+	    arr.sort()
