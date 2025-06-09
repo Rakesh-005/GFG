8c090485251e02a070993d14aaf8b82363186cec
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-06-10 00:41:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Arithmetic Number/arithmetic-number.cpp
@@ -0,0 +1,13 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int inSequence(int A, int B, int C) {
+        // code here
+         if(C==0){
+            return A==B;
+        }
+       return (B-A)%C==0 && (B-A)/C>=0;
+       
+    }
+};
