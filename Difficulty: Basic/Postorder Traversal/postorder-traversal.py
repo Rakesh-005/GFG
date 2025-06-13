66b529c6a2031dc35ce8fdf09285096bbf4e1e29
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-06-13 17:34:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Postorder Traversal/postorder-traversal.py
@@ -0,0 +1,15 @@
+'''
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+class Solution:
+    #Function to return a list containing the postorder traversal of the tree.
+    def postOrder(self, root):
+        # code here
+        if not root:
+            return []
+        return self.postOrder(root.left)+self.postOrder(root.right)+[root.data]
