45e3e4ecc99082a479ea4f923cf974bb392e5d52
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-06-13 12:59:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Sum Increasing Subsequence/max-sum-increasing-subsequence.py
@@ -0,0 +1,14 @@
+#User function Template for python3
+class Solution:
+	def maxSumIS(self, arr):
+		# code here
+		n=len(arr)
+		dp, ans = arr[:], arr[0]
+        
+        for i in range(1, n):
+            for j in range(i):
+                if arr[i]>arr[j] and dp[i]<arr[i]+dp[j]:
+                    dp[i] = arr[i]+dp[j]
+            ans = max(ans, dp[i])
+        
+        return ans
