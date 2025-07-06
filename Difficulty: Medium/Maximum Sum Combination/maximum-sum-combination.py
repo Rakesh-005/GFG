d552891b88496207ba7569c811f4c9fcae337901
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-07-06 11:36:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.py
@@ -0,0 +1,19 @@
+class Solution:
+    def topKSumPairs(self, a, b, k):
+        from heapq import heapify, heappop, heappush
+        n = len(a)
+        # Build-in heap is a minheap, so we negate arrays
+        for i in range(n):
+            a[i] *= -1
+            b[i] *= -1
+        a.sort()
+        b.sort()
+        h = [(a[i] + b[0], i, 0) for i in range(n)]
+        heapify(h)
+        output = []
+        while len(output) < k:
+            min_sum, i, j = heappop(h)
+            output.append(-min_sum)
+            if j < n - 1:
+                heappush(h, (a[i] + b[j + 1], i, j + 1))
+        return output
