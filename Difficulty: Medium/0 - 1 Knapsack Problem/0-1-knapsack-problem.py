52a7d6958abbc1fd9b243d324c4ff11e8f0f6510
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-07-30 01:10:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -1,25 +1,16 @@
 class Solution:
-    def knapsack(self, W, val, wt):
-        # code here
-        n = len(wt)
-        dp = [[0]*(n+1) for _ in range(W+1)]
-        for w in range(1, W+1):
-            for i in range(n):
-                dp[w][i+1] = dp[w][i]
-                if wt[i] <= w:
-                    dp[w][i+1] = max(dp[w][i+1], dp[w-wt[i]][i]+val[i])
-        return dp[-1][-1]
-
-
-#{ 
- # Driver Code Starts
-if __name__ == '__main__':
-    test_cases = int(input())
-    for _ in range(test_cases):
-        capacity = int(input())
-        values = list(map(int, input().strip().split()))
-        weights = list(map(int, input().strip().split()))
-        ob = Solution()
-        print(ob.knapsack(capacity, values, weights))
-        print("~")
-# } Driver Code Ends
+    # Function to find the maximum profit
+    def knapsack(self,W, val, wt):
+        
+        # Initializing dp list
+        dp = [0] * (W + 1)
+    
+        # Taking first i elements
+        for i in range(1, len(wt) + 1):
+            
+            # Starting from back, so that we also have data of
+            # previous computation of i-1 items
+            for j in range(W, wt[i - 1] - 1, -1):
+                dp[j] = max(dp[j], dp[j - wt[i - 1]] + val[i - 1])
+        
+        return dp[W]
