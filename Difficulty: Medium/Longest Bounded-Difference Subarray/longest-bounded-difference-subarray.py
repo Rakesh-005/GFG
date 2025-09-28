56faadf4814b086fe66362c2e276ead412a5343d
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-09-28 23:19:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.py
+++ Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.py
@@ -22,19 +22,3 @@ class Solution:
         return arr[st:en+1] if en > st else [arr[0]]
 
 
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-if __name__ == '__main__':
-    tc = int(input())
-    while tc > 0:
-        arr = list(map(int, input().strip().split()))
-        k = int(input().strip())
-        ob = Solution()
-        ans = ob.longestSubarray(arr, k)
-
-        print(" ".join(map(str, ans)))
-        tc -= 1
-        print("~")
-
-# } Driver Code Ends
