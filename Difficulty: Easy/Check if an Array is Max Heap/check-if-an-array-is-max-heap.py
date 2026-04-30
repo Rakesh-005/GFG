2818b1a9343cc69ff13fc25656adf85e73c270a4
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2026-04-30 20:07:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if an Array is Max Heap/check-if-an-array-is-max-heap.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def isMaxHeap(self,arr):
+        # Your code goes here
+        n=len(arr)
+        for i in range(n//2):
+            l=2*i+1
+            r=2*i+2
+            if (l<n and arr[l]>arr[i]) or (r<n and arr[r]>arr[i]):
+                return False
+        return True
+
+
